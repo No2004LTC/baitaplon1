271f33f6b6b1695cc807324f4db9c919403508f2
LeThanhCong <daithan2k4@gmai.com>
2025-04-24 02:37:02 +0000

    nhanh 1

--- configs/config.php
+++ configs/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('_WEB_HOST', 'http://'. $_SERVER['HTTP_HOST'] .'/baitapnew');
+define('_WEB_HOST', 'http://'. $_SERVER['HTTP_HOST'] );
 
 //database
 const _HOST = 'localhost';
 const _DB = 'baitapnew';
 const _USER = 'root';
-const _PASS = '';
+const _PASS = '1';
